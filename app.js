b29301521df86123d1319cc8a081d1edba1a99e6
Joseph Martucci <joe.martucci@gmail.com>
2026-02-05 19:10:29 -0500

    And only reply in threads.

--- app.js
+++ app.js
@@ -108,7 +108,6 @@ app.message(".gif", async ({ message, client }) => {
   if (gifs.length === 0) {
     await client.chat.postEphemeral({
       channel: message.channel,
-      thread_ts: thread_ts,
       user: message.user,
       text: `No gifs found for "${searchTerm}"`,
     });
@@ -134,7 +133,6 @@ app.message(".gif", async ({ message, client }) => {
   try {
     await client.chat.postEphemeral({
       channel: message.channel,
-      thread_ts: thread_ts,
       user: message.user,
       text: `Preview for "${searchTerm}"`,
       blocks: [
